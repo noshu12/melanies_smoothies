14c6a1e1f6867452fe779420b085b349d4417cb1
NOUSHAD ALAM <135364309+noshu12@users.noreply.github.com>
2026-02-12 02:48:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,10 @@ pd_df=my_dataframe.to_pandas()
 
 # Multi-select for ingredients
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    my_dataframe.select(col('FRUIT_NAME')), # Select only the name column
-    max_selections=5
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
 )
-
 
 if ingredients_list:
     ingredients_string = ''
